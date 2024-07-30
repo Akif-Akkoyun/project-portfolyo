13bf5317f43d513e93fef019c5d0e6b60fd25cb1
Akif Akkoyun <144811507+Akif-Akkoyun@users.noreply.github.com>
2024-07-30 04:10:42 +0300

    Create main project

---
+++ Project/App.Portfolyo/App.Portfolyo.Mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace App.Portfolyo.Mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
